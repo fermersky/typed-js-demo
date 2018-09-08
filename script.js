e7fefd5e7452013d5bf4067e7fb2779888d79b45
Daniel Skrypnik <forte201253@gmail.com>
2018-09-08 19:37:46 +0300

    Added project files

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+
+var typed = new Typed('#typed', {
+    strings: [
+      "`MacBook-Pro:typedjs daniel ` ^2000 $ git clone git@github.com:fermersky/typed-js-demo.git ^500\n <br> `Cloning into 'dz2'...` ^3000\n <br> `remote: Counting objects: 89, done.`<br>  `remote: Total 89 (delta 0), reused 0 (delta 0), pack-reused 89`<br> `Receiving objects: 100% (89/89), 795.53 KiB | 777.00 KiB/s, done.`<br> `Resolving deltas: 100% (31/31), done.` <br> `MacBook-Pro:typedjs daniel $`"
+    ],
+    smartBackspace: true,
+    typeSpeed: 50,
+    backSpeed: 10
+  });
